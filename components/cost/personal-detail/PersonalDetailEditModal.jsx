3c074f3f1f4a1b5ba4893f627a2b93fc4332cbc8
alsgkr3894-bit <alsgkr3894@gmail.com>
2026-06-15 13:26:14 +0900

    refactor: remove unused legacy cost editors

--- components/cost/personal-detail/PersonalDetailEditModal.jsx
+++
@@ -1,7 +0,0 @@
-'use client';
-import { personalTotalCost } from '@/lib/cost/personal-detail';
-import { DetailEditModal } from '@/components/cost/shared/DetailEditModal';
-
-export function PersonalDetailEditModal(props) {
-  return <DetailEditModal {...props} calcCost={personalTotalCost} listIdPrefix="personal-detail" />;
-}
